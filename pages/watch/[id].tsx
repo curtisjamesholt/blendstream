bfb0c359aec92841292872fb061659e7f6bdfeb9
Joshua Knauber <joshua.knauber@gmail.com>
2023-01-05 02:42:58 +0100

    fix: removed old prop

--- pages/watch/[id].tsx
+++ pages/watch/[id].tsx
@@ -1,14 +1,14 @@
-import { useRouter } from "next/router";
-import useMovie from "../../src/hooks/useMovie";
-import ReactPlayer from "react-player";
-import Link from "next/link";
-import Head from "next/head";
+import { useRouter } from 'next/router';
+import useMovie from '../../src/hooks/useMovie';
+import ReactPlayer from 'react-player';
+import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Player() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { movie, ytThumnailUrl } = useMovie(typeof id === "string" ? id : "");
+  const { movie } = useMovie(typeof id === 'string' ? id : '');
 
   if (!movie) return <div>loading...</div>;
 
@@ -17,15 +17,15 @@ export default function Player() {
       <Head>
         <title>{movie.title}</title>
       </Head>
-      <div style={{ overflow: "hidden" }}>
-        <div style={{ position: "fixed", top: "50%", left: 0 }}>
+      <div style={{ overflow: 'hidden' }}>
+        <div style={{ position: 'fixed', top: '50%', left: 0 }}>
           <Link href={`/movies/${movie.id}`}>Back</Link>
         </div>
         <ReactPlayer
           url={movie.url}
           playing={true}
-          width={"100vw"}
-          height={"100vh"}
+          width={'100vw'}
+          height={'100vh'}
           light
           controls
         />
