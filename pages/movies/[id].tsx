7ce28c50edffbfc7e4525dceb1d228471715ad6a
Joshua Knauber <joshua.knauber@gmail.com>
2023-01-05 15:28:53 +0100

    feat: mobile support movie page

--- pages/movies/[id].tsx
+++ pages/movies/[id].tsx
@@ -9,6 +9,7 @@ import Header from '../../src/components/layout/Header';
 import Image from 'next/image';
 import { FiHeart, FiPlay, FiPlus } from 'react-icons/fi';
 import { useSession } from '@supabase/auth-helpers-react';
+import Footer from '../../src/components/layout/Footer';
 
 export default function Movie() {
   const router = useRouter();
@@ -36,12 +37,12 @@ export default function Movie() {
       </Head>
       <>
         <Header />
-        <div>
+        <div className="h-[100vh]">
           <Link href={`/watch/${movie.id}`}>
-            <div className="relative mx-8 h-[80vh] overflow-hidden rounded-xl">
+            <div className="relative h-[80vh] overflow-hidden md:mx-8 md:mt-4 md:rounded-xl">
               {thumbnail && (
                 <Image
-                  className="aspect-video object-cover"
+                  className="aspect-video object-cover gradient-mask-b-50 md:[mask-image:none]"
                   src={thumbnail}
                   alt="Thumbnail"
                   fill
@@ -55,11 +56,11 @@ export default function Movie() {
                 }}
               ></div>
 
-              <div className="absolute top-1/2 left-1/2 flex aspect-square w-[70px] items-center justify-center rounded-full bg-black bg-opacity-40 transition-all hover:bg-opacity-70">
+              <div className="absolute top-1/2 left-1/2 flex aspect-square w-[70px] translate-x-[-50%] translate-y-[-50%] items-center justify-center rounded-full bg-black bg-opacity-40 transition-all hover:bg-opacity-70">
                 <FiPlay className="ml-1 fill-white" size={24} />
               </div>
 
-              <div className="absolute left-8 bottom-8">
+              <div className="absolute left-4 bottom-4 md:left-8 md:bottom-8">
                 <span className="text-5xl font-semibold">{movie.title}</span>
                 <div className="mt-2 flex items-center gap-4">
                   <div className="relative aspect-square w-6 overflow-hidden rounded-full">
@@ -76,14 +77,14 @@ export default function Movie() {
                   <div className="mt-6 flex gap-2">
                     <button
                       onClick={onToggleWatchlist}
-                      className="flex items-center gap-4 rounded-md bg-white bg-opacity-10 px-4 py-2 text-sm font-medium backdrop-blur-sm"
+                      className="flex items-center gap-4 rounded-md bg-white bg-opacity-10 px-4 py-2 text-sm font-medium backdrop-blur-sm transition-all hover:bg-opacity-20"
                     >
                       <FiPlus strokeWidth={3} />
                       Add to Watchlist
                     </button>
                     <button
                       onClick={onToggleFavorite}
-                      className="flex items-center gap-4 rounded-md bg-white bg-opacity-10 px-4 py-2 text-sm font-medium backdrop-blur-sm"
+                      className="flex items-center gap-4 rounded-md bg-white bg-opacity-10 px-4 py-2 text-sm font-medium backdrop-blur-sm transition-all hover:bg-opacity-20"
                     >
                       <FiHeart strokeWidth={3} />
                       Favorite
@@ -94,6 +95,7 @@ export default function Movie() {
             </div>
           </Link>
         </div>
+        <Footer />
       </>
     </>
   );
