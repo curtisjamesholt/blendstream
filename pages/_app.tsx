f838c936d5628965ff61212fef8454a82c1a0cc9
Joshua Knauber <joshua.knauber@gmail.com>
2023-01-19 14:06:17 +0100

    added posthog (testing)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,11 @@ import { Inter } from '@next/font/google';
 import type { AppProps } from 'next/app';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import Head from 'next/head';
+import posthog from 'posthog-js';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +15,12 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
+
+  useEffect(() => {
+    posthog.init('phc_ndZDodibehHFJArgqFdPFUOlk7Kvq7Ed2qId45nBYZv', {
+      api_host: 'https://eu.posthog.com',
+    });
+  }, []);
 
   return (
     <>
@@ -27,6 +35,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:url" content="https://blend.stream" />
         <meta property="og:image" content="/embed_image.jpg" />
       </Head>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+          posthog.init('phc_ndZDodibehHFJArgqFdPFUOlk7Kvq7Ed2qId45nBYZv',{api_host:'https://eu.posthog.com'})`,
+        }}
+      />
       <div className={inter.className}>
         <SessionContextProvider
           supabaseClient={supabase}
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { posthog } from 'posthog-js';
 import useMovieThumbnail from '../hooks/useMovieThumbnail';
 import { Movie } from '../hooks/usePublishMovie';
 
@@ -12,8 +13,15 @@ const MovieCard = (props: MovieCardProps) => {
 
   const { mid: thumbnail } = useMovieThumbnail(movie);
 
+  const onCardClick = () => {
+    posthog.capture('movie card clicked', {
+      movieTitle: movie.title,
+      movieId: movie.id,
+    });
+  };
+
   return (
-    <Link href={`/movies/${movie.id}`}>
+    <Link href={`/movies/${movie.id}`} onClick={onCardClick}>
       <div className="relative aspect-video w-full overflow-hidden rounded-md md:transition-transform md:hover:scale-105">
         {thumbnail && (
           <Image
