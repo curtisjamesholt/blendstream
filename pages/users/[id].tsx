8736c007fd094fc061da31ff52328896e2460b5e
Joshua Knauber <joshua.knauber@gmail.com>
2023-01-28 14:35:54 +0100

    slightly improved user page

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -3,11 +3,13 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FaGlobe, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { FastAverageColor } from 'fast-average-color';
 import Footer from '../../src/components/layout/Footer';
 import Header from '../../src/components/layout/Header';
 import MovieSlider from '../../src/components/layout/MovieSlider';
 import { useMoviesByCreator } from '../../src/hooks/useMovies';
 import useUser from '../../src/hooks/useUser';
+import { useEffect, useRef, useState } from 'react';
 
 const socialLink =
   'flex flex-row items-center gap-2 w-min opacity-60 hover:opacity-100 transition-opacity';
@@ -18,6 +20,10 @@ export default function User() {
 
   const { profile } = useUser(typeof id === 'string' ? id : '');
   const { movies } = useMoviesByCreator(typeof id === 'string' ? id : '');
+
+  const [bgColor, setBgColor] = useState<[number, number, number]>([0, 0, 0]);
+
+  const imgRef = useRef<HTMLImageElement>(null);
 
   const isValidUrl = (url: string, origin?: string) => {
     try {
@@ -31,6 +37,20 @@ export default function User() {
       : true;
   };
 
+  const getBackgroundColor = async () => {
+    console.log('try');
+    if (imgRef.current) {
+      console.log('ye');
+      const fac = new FastAverageColor();
+      const color = await fac.getColorAsync(imgRef.current);
+      setBgColor([color.value[0], color.value[1], color.value[2]]);
+    }
+  };
+
+  useEffect(() => {
+    getBackgroundColor();
+  }, [profile, imgRef.current]);
+
   return (
     <>
       <Head>
@@ -38,30 +58,19 @@ export default function User() {
       </Head>
       <div className="flex min-h-[100vh] flex-col">
         <Header />
-        <div className="fixed top-0 left-0 h-[500px] w-full overflow-hidden opacity-70">
-          {profile?.profile_picture && (
-            <Image
-              src={profile.profile_picture}
-              alt="Profile Picture"
-              className="ph-no-capture w-full scale-110 object-cover blur-xl"
-              height={250}
-              width={250}
-            />
-          )}
-          <div
-            className="absolute top-0 left-0 h-full w-full"
-            style={{
-              background:
-                'linear-gradient(0deg, rgba(0,0,0,1) 5%, rgba(0,0,0,0.5) 100%)',
-            }}
-          ></div>
-        </div>
+        <div
+          className="fixed top-0 left-0 h-3/4 w-full overflow-hidden opacity-70 transition-all"
+          style={{
+            background: `linear-gradient(0deg, rgba(${bgColor[0]}, ${bgColor[1]}, ${bgColor[2]}, 0) 0%, rgba(${bgColor[0]}, ${bgColor[1]}, ${bgColor[2]}, .25) 100%)`,
+          }}
+        ></div>
         <div className="z-10 mx-4 mt-8 flex-grow pt-8 md:mx-8">
           <div className="mx-8 flex flex-col gap-4">
             <div className="relative aspect-square w-[100px] overflow-hidden rounded-full">
               {profile?.profile_picture && (
                 <Image
                   src={profile.profile_picture}
+                  ref={imgRef}
                   width={250}
                   height={250}
                   className="ph-no-capture h-full w-full object-cover"
